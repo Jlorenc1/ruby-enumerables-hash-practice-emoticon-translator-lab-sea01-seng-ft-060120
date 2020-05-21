a71a9a245b8839256e58648b4f36d895a84bdbca
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-21 15:08:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,8 @@ def load_library(path)
   
   emoticons.each do |key, value|
     emoticons_hash[key] = {:english => value[0], :japanese => value[1]}
-  
+  end
+  emoticons_hash
 end
 
 def get_japanese_emoticon
