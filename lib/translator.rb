cdbcd0fe5e4cf751347d75ef733233825c105144
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-21 15:08:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(path)
   emoticons = YAML.load_file(path)
   
   emoticons.each do |key, value|
-    emoticons_hash[key] = {:english 
+    emoticons_hash[key] = {:english => value[0], :japanese => value[1]}
   
 end
 
