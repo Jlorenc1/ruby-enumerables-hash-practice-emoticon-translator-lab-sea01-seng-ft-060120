d46c89b420bd2dcf009cbed4498f71d22b50fe5e
Joshua Lorenc <joshua.lorenc@icloud.com>
2020-05-21 15:49:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,6 @@ def get_japanese_emoticon(file_path, emoticon)
   emoticon_library = load_library(file_path)
   emoticon_library.each do |key, value|
     return value[:japanese] if value[:english] == emoticon
-    end
   end
   "Sorry, that emoticon was not found"
 end
@@ -24,7 +23,6 @@ def get_english_meaning(file_path, emoticon)
   emoticon_library = load_library(file_path)
   emoticon_library.each do |key, value|
     return value[:english] if value[:japanese] == emoticon
-    end
   end
   "Sorry, that emoticon was not found"
 end
